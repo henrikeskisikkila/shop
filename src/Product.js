dd0147cc30f9cbb6d38c30051113501da10d1707
Henri Keski-Sikkila <henri@keskisikkila.fi>
2022-03-03 09:59:31 +0200

    Add product view

--- src/Product.js
+++ src/Product.js
@@ -1,21 +1,48 @@
-import { Box } from "@mui/material";
+import {
+  Grid,
+  Card,
+  CardContent,
+  Typography,
+  CardMedia,
+  CardActions,
+  Button,
+} from "@mui/material";
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
+
+const product = {
+  name: "Apple iPhone 13 256 Gt",
+  description:
+    "Apple iPhone 13. Kaikkein edistynein iPhonen kaksoiskamera­järjestelmä. Salaman­nopea A15 Bionic ‑siru. Iso harppaus akunkestossa.",
+  price: "948,90",
+  barcode: "5901234123457",
+  image: "5901234123457.jpeg",
+};
 
 function Product() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
+    <Grid container direction="column" alignItems="center" justify="center">
+      <Card sx={{ width: 600 }}>
+        <CardContent>
+          <Typography variant="h5">{product.name}</Typography>
+          <CardMedia
+            component="img"
+            sx={{ maxWidth: 300, margin: 4 }}
+            image={`${product.image}`}
+            alt={product.name}
+          />
+          <Typography variant="body2">{product.description}</Typography>
+          <Typography variant="h6" style={{ color: "red", fontWeight: "bold" }}>
+            {product.price}
+          </Typography>
 
-        bgcolor: "background.paper",
-        borderRadius: "2px",
-        boxShadow: 1,
-      }}
-    >
-      <Box sx={{ width: 600 }}> content </Box>
-    </Box>
+          <CardActions>
+            <Button variant="contained" startIcon={<AddShoppingCartIcon />}>
+              Add
+            </Button>
+          </CardActions>
+        </CardContent>
+      </Card>
+    </Grid>
   );
 }
 
